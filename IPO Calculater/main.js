71d42a1123437bbb718542da1ec17fff256bb8e0
df9ui <145046261+df9ui@users.noreply.github.com>
2023-10-18 11:21:37 -0600

    Add files via upload

---
+++ IPO Calculater/main.js
@@ -0,0 +1,11 @@
+document.getElementById("btn").addEventListener("click", btnClicked);
+function btnClicked() {
+  let a = +document.getElementById("a-in").value;
+  let b = +document.getElementById("b-in").value;
+  let c = +document.getElementById("c-in").value;
+
+  let s = (a + b + c) / 2;
+  let A = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+  document.getElementById("s").innerHTML = `semi-perimeter=${s}`;
+  document.getElementById("A").innerHTML = `Area=${Math.round(A)}`;
+}
