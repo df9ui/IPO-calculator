9d2e3dbd224f815229643270cf21b9aa9bf4885e
df9ui <145046261+df9ui@users.noreply.github.com>
2023-10-19 10:27:01 -0600

    Update main.js

--- IPO Calculater/main.js
+++ IPO Calculater/main.js
@@ -5,7 +5,8 @@ function btnClicked() {
   let c = +document.getElementById("c-in").value;
 
   let s = (a + b + c) / 2;
-  let A = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+  let v = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+  let A=v.toFixed(1);
   document.getElementById("s").innerHTML = `semi-perimeter=${s}`;
   document.getElementById("A").innerHTML = `Area=${Math.round(A)}`;
 }
